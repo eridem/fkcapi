799bb5e70ff25bd42ddf463c62f83b2d6afa2f42
eridem <eridem@5cc3c110-e9ed-47b1-9b94-94b59c022222>
2009-05-04 07:50:48 +0000

    Add licence to the whole files. Without any more change.

--- FkcUser.class.php
+++ FkcUser.class.php
@@ -1,4 +1,25 @@
 <?php
+/**
+    This file is part of Friendly Korea Community API.
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Friendly Korea Community API is distributed in the hope that it will
+    be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Friendly Korea Community API.
+    If not, see <http://www.gnu.org/licenses/>.
+
+    Miguel Ángel Domínguez Coloma (eridem@gmail.com)
+    http://eridem.net/friendly-korea-community-api/
+*/
+
 class FkcUser {
 
 	private $id;
